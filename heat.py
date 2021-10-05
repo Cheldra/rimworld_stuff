5ed3ce23831ddea8c1716d096bc9390f86ee86f6
Cheldra <Cheldra@users.noreply.github.com>
2021-10-05 15:06:44 +0100

    code for graph and table

--- heat.py
+++ heat.py
@@ -1,16 +1,35 @@
 import curve
 
-def on_interval_passed(ambient_temp, max_comfortable_temp=26):
+def on_interval_passed(degrees_above_comfortable):
     # based on Verse.HediffGiver_Heat.OnIntervalPassed
-    difference = ambient_temp - (max_comfortable_temp + 10)
+    difference = degrees_above_comfortable - 10
+    if difference < 0:
+        return 0
     modified_difference = curve.post_process_curve(difference,
                                                    x=[0, 25, 50, 100, 200, 400, 4000],
                                                    y=[0, 25, 40, 60, 70, 100, 1000])
-    a = modified_difference*6.45e-5
-    min_a = max(a, 0.000375)
-    print(difference, modified_difference, round(a, 6), round(min_a, 6))
-    return a
+    growth_per_60_ticks = modified_difference*6.45e-5
+    growth_per_60_ticks = max(growth_per_60_ticks, 0.000375)
+    return growth_per_60_ticks
 
+x = []
+y = []
+for temp_difference in range(10, 101):
+    if temp_difference % 2 != 0:
+        continue
+    ticks = 1/on_interval_passed(temp_difference)*60
+    result = round(ticks/2500, 1)
+    print(temp_difference, result)
+    x.append(str(temp_difference))
+    y.append(str(result))
 
-for temp in range(26, 46):
-    on_interval_passed(temp)
+print(', '.join(x))
+print(', '.join(y))
+
+for temp_difference in [10, 15, 20, 25, 100, 300, 4000]:
+    growth_per_60_ticks = on_interval_passed(temp_difference)
+    ticks = 1/growth_per_60_ticks*60
+    secs = round(ticks/60, 1)
+    in_game_hour = round(ticks/2500, 1)
+    in_game_day = round(ticks/60000, 1)
+    print(temp_difference, round(growth_per_60_ticks, 6), round(ticks, 1), secs, in_game_hour, in_game_day)
